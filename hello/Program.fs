745551d285dcd5ed28f0d211c0b8c69d0e7c74d6
CG <m.jason.liu@outlook.com>
2016-12-10 04:40:20 +0800

    origin

---
+++ hello/Program.fs
@@ -0,0 +1,28 @@
+﻿
+[<EntryPoint>]
+let main args= 
+    let str = stdin.ReadLine()
+
+    for i = 0 to str.Length-1 do
+        if str.[i].CompareTo('+') = 0 then
+            let sub1:int = Core.int.Parse str.[0..i-1]
+            let sub2:int = Core.int.Parse str.[i+1..str.Length-1]
+            printfn "%A" (sub1+sub2)
+
+        if str.[i].CompareTo('-') = 0 then
+            let sub1:int = Core.int.Parse str.[0..i-1]
+            let sub2:int = Core.int.Parse str.[i+1..str.Length-1]
+            printfn "%A" (sub1-sub2)
+
+        if str.[i].CompareTo('*') = 0 then
+           let sub1:int = Core.int.Parse str.[0..i-1]
+           let sub2:int = Core.int.Parse str.[i+1..str.Length-1]
+           printfn "%A" (sub1*sub2)
+
+        if str.[i].CompareTo('/') = 0 then
+           let sub1:int = Core.int.Parse str.[0..i-1]
+           let sub2:int = Core.int.Parse str.[i+1..str.Length-1]
+           printfn "%A" (sub1/sub2)
+
+    stdin.Read()
+    0
